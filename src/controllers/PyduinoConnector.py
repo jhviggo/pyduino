9d26d6db7b504a66059544b5303d1ab31875dc04
Viggo Petersen <vig56@hotmail.com>
2018-10-24 20:48:19 +0200

    Refactor pyduino controller
    To make pyduino easier to use i have added controllers to connect and
    interact with the arduino. Each method is documentet with parameters and
    return type, handles errors and returns values or booleans.
    
    - add PyduinoConnector
    - - add method documentation
    - - add error handling
    - - add return statements
    
    - add PyduinoController
    - - add method documentation
    - - add error handling
    - - add return statements

---
+++ src/controllers/PyduinoConnector.py
@@ -0,0 +1,43 @@
+import serial
+
+
+class PyduinoConnector:
+    def __init__(self, serial_port='COM6', baud_rate=9600, read_timeout=5, verbose=False):
+        self.conn = None
+        self.serial_port = serial_port
+        self.read_timeout = read_timeout
+        self.baud_rate = baud_rate
+        self.verbose = verbose
+
+    def connect_to_serial_port(self):
+        """
+        Connects to a serial port COM2, COM3, COM6 TTY...
+
+        :return: Serial object
+        """
+        if self.verbose:
+            print('Connecting to port', self.serial_port + '...')
+        try:
+            self.conn = serial.Serial(self.serial_port, self.baud_rate)
+            self.conn.timeout = self.read_timeout
+            return self.conn
+        except Exception as e:
+            print('\t[-]', e)
+            return None
+
+    def close_connection(self):
+        """
+        Close connection to serial port
+
+        :return: boolean
+        """
+        if self.verbose:
+            print('Closing connection...')
+        try:
+            self.conn.close()
+            if self.verbose:
+                print('\t', 'connection closed')
+            return True
+        except Exception as e:
+            print('\t[-]', e)
+            return False
---
+++ src/controllers/PyduinoController.py
@@ -0,0 +1,146 @@
+
+class PyduinoController:
+    def __init__(self, conn, verbose=False):
+        self.conn = conn
+        self.verbose = verbose
+        self.currentActivePins = []
+        self.pin_modes = {
+            'OUTPUT': 'O',
+            'INPUT': 'I',
+            'INPUT_PULLUP': 'P'
+        }
+
+    def set_pin_mode(self, pin_number, mode='OUTPUT'):
+        """
+        sets a pin with a mode
+
+        :param pin_number:  int
+        :param mode: string specified in self.pin_modes
+        :return: boolean
+        """
+        if self.verbose:
+            print('Setting pin mode')
+
+        if mode not in self.pin_modes.keys():
+            print('\t[-]', 'unknown pin mode')
+            return False
+
+        try:
+            command = (''.join(('M', self.pin_modes[mode], str(pin_number)))).encode()
+            if self.verbose:
+                print('\t', 'setting pin', pin_number, 'as', mode)
+            self.conn.write(command)
+            if self.verbose:
+                print('\t', 'saving as {"pin": ' + str(pin_number) + ', "mode": "' + mode + '"} to local storage')
+            self.currentActivePins.append({'pin': pin_number, 'mode': mode})
+            return True
+        except Exception as e:
+            print('\t[-]', e)
+            return False
+
+    def set_all_pin_modes(self, pins=()):
+        """
+        Sets a list of pins with pin modes
+
+        :param pins: tuple of dictionaries
+        :return: boolean
+        """
+        if len(pins) is 0:
+            print('\t[-]', 'no pins specified')
+
+        for i in pins:
+            if not self.set_pin_mode(i['pin'], i['mode']):
+                return False
+        return True
+
+    def digital_write(self, pin_number, digital_value):
+        """
+        Digital write to a pin with data
+
+        :param pin_number: int
+        :param digital_value: int
+        :return: boolean
+        """
+        if self.verbose:
+            print('Writing to digital pin')
+
+        try:
+            command = (''.join(('WD', str(pin_number), ':', str(digital_value)))).encode()
+            if self.verbose:
+                print('\t', 'writing', digital_value, 'to digital pin', pin_number)
+            self.conn.write(command)
+            return True
+        except Exception as e:
+            print('\t[-]', e)
+            return False
+
+    def digital_read(self, pin_number):
+        """
+        Digital read on a pin
+
+        :param pin_number: int
+        :return: int
+        """
+        if self.verbose:
+            print('Reading from digital pin')
+
+        try:
+            command = (''.join(('RD', str(pin_number)))).encode()
+            self.conn.write(command)
+            if self.verbose:
+                print('\t', 'reading from digital pin', pin_number)
+            line_received = self.conn.readline().decode().strip()
+            header, value = line_received.split(':')
+        except Exception as e:
+            print('\t[-]', e)
+            return -1
+
+        if header and header == ('D' + str(pin_number)):
+            return int(value)
+        return -1
+
+    def analog_write(self, pin_number, analog_value):
+        """
+        Analog write to a pin with data
+
+        :param pin_number: int
+        :param analog_value: int
+        :return: boolean
+        """
+        if self.verbose:
+            print('Writing to analog pin')
+
+        try:
+            command = (''.join(('WA', str(pin_number), ':', str(analog_value)))).encode()
+            if self.verbose:
+                print('\t', 'writing', analog_value, 'to analog pin', pin_number)
+            self.conn.write(command)
+            return True
+        except Exception as e:
+            print('\t[-]', e)
+            return False
+
+    def analog_read(self, pin_number):
+        """
+        Analog read on a pin. Arduino can only return from 0 to 1023
+
+        :param pin_number: int
+        :return: boolean
+        """
+        if self.verbose:
+            print('Reading from analog pin')
+
+        try:
+            command = (''.join(('RA', str(pin_number)))).encode()
+            self.conn.write(command)
+            if self.verbose:
+                print('\t', 'reading from analog pin', pin_number)
+            line_received = self.conn.readline().decode().strip()
+            header, value = line_received.split(':')
+        except Exception as e:
+            print('\t[-]', e)
+            return -1
+
+        if header == ('A' + str(pin_number)):
+            return int(value)
+        return -1

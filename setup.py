aa13e12659116342fc42d241b2295454e764e3a8
Viggo Petersen <viggo.petersen@sallinggroup.com>
2019-11-21 22:21:11 +0100

    Add package setup
    - add setup file with setuptools
    - add dist and egg-info to .gitignore

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyduino",
+    version="1.0.0",
+    description="Python package for controllering arduinos over serial",
+    author="Viggo Petersen",
+    license="MIT",
+    packages=find_packages()
+)
